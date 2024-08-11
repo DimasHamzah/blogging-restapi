599d7acd7e10c10906ade93da9e06fcfdb23d860
dimas nurhamzah <hamzahdimas85@gmail.com>
2024-08-10 18:42:49 -0700

    add middleware

--- app/controllers/authenticationController.js
+++ app/controllers/authenticationController.js
@@ -57,7 +57,7 @@ module.exports = {
             const { username, email, password } = req.body;
             const errors = validationResult(req);
 
-            if (errors.isEmpty()) {
+            if (!errors.isEmpty()) {
                 return res.status(400).json({errors: errors.array()});
             }
 
